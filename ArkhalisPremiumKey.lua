81a46e56a7c367d7fa197e08bd24532983311989
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-04-26 17:50:11 +0200

    Update ArkhalisPremiumKey.lua

--- ArkhalisPremiumKey.lua
+++ ArkhalisPremiumKey.lua
@@ -7,8 +7,9 @@ local counter = 1
 local keyCheck
 for i,v in pairs(keys) do
     if counter == #keys then
-    game.Players.LocalPlayer:Kick("Please Use Correct Key")
+    --game.Players.LocalPlayer:Kick("Please Use Correct Key")
     keys = ""
+        print("no")
     else
         if v == _G.Key then
             --Whitelisted!
