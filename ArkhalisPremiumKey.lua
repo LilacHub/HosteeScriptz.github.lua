03cd4bee5ad812a23033267db774f2d69f74e09f
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-04-26 17:40:12 +0200

    Update ArkhalisPremiumKey.lua

--- ArkhalisPremiumKey.lua
+++ ArkhalisPremiumKey.lua
@@ -2,3 +2,22 @@ local keys = {
     "ARKHALIS-DWETRLOAWOBP", --// Arkhalis
     "ARKHALIS-VQCNYSIPDRSA" --// OWNER
 }
+
+local counter = 1
+local keyCheck
+for i,v in pairs(keys) do
+    if counter == #keys then
+    --game.Players.LocalPlayer:Kick("Please Use Correct Key")
+    keys = ""
+    print("no")
+    else
+        if v == _G.Key then
+            --Whitelisted!
+            print("Successfully whitelisted!")
+            keyCheck = _G.Key
+            keys = ""
+        else
+            counter = counter +1
+        end
+    end
+end
