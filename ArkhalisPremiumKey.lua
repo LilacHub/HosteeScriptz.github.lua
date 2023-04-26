e41821aa5ed45220c79f7153e16505678565d473
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-04-26 16:04:34 +0200

    Update ArkhalisPremiumKey.lua

--- ArkhalisPremiumKey.lua
+++ ArkhalisPremiumKey.lua
@@ -2,9 +2,9 @@ return {
   ["toggle"] = {3307344083, false},
   ["rm"] = {3, 18, 5, 16, {5, 26}, 3, 18, 9, 16, {2, 20}},
   ["whitelist"] = {
-    ["K41zDev"] = "ARKHALIS-DWETRLOAWOBP,"
+    ["K41zDev"] = "ARKHALIS-DWETRLOAWOBP",
    },
   ["Premium"] = {
-    ["Arkhalisowner"] = "ARKHALIS-VQCNYSIPDRSA,"
+    ["Arkhalisowner"] = "ARKHALIS-VQCNYSIPDRSA",
     }
 }
