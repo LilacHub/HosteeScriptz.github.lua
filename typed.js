3e44372193531433eabdc72186180e283ef8141f
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-05-01 20:18:07 +0200

    Delete typed.js

--- typed.js
+++
@@ -1,9 +0,0 @@
-new Typed('#typed', {
-    strings: ['Lua, Python'],
-    typeSpeed: 480,
-    backSpeed: 480,
-    backDelay: 1000,
-    startDelay: 120,
-    loop: true,
-    showCursor: false
-});
